a7f8ebdb8765d22ee97aed5b1b9f9da2f3956eb3
rj3838 <roy.jaques@live.co.uk>
2025-03-17 17:29:50 +0000

    left in a position where the grid data is grouped by sectionID and sectionNR

--- CVItoHMDIF.jl
+++ CVItoHMDIF.jl
@@ -8,11 +8,30 @@ using NativeFileDialog
 using FilePathsBase
 using CSV
 using DataFrames
+using DataFramesMeta
 
 function fn_hmd_cvi_data_records(grid_data)
 
+    #using DataFrames
+    # drop rows with missing fields, this drop the dummy data at the end of the file.
     dropmissing!(grid_data)
-    print(grid_data)
+
+    # rename the 'Section ID' by removing the spaces as it's easier to deal with.
+    rename!(grid_data, Symbol("Section ID") => :SectionID)
+    
+    # convert the chainage to a float then
+    # create a new column using flooring division on the Chaniage to produce a 'new' section number each 20m
+    # then convert it to a string.
+    transform!(grid_data, :Chainage => ByRow(x -> parse(Float64, x)) => :Chainage)
+    @rtransform!(grid_data, :sectionNr = fld1(:Chainage, 20))
+    @rtransform!(grid_data, :sectionNr = string(round(Int, :sectionNr)))
+
+    # Section Id and sectionNr can now be used to create a grouped data frame.
+    gdf_grid_data = groupby(grid_data, [:SectionID, :sectionNr])
+
+    # now for each grouped DF create records for section + observ + obval 
+
+    println(gdf_grid_data)
     return 
 end
 
@@ -57,7 +76,7 @@ data_out = fn_hmd_cvi_data_records(grid_data)
 
     HMDIF_count = size(HMDIF_out)[1]
     dend_count = Int(HMDIF_count) - 6
-    println(dend_count)
+    #println(dend_count)
     #DEND_string = raw"DEND\\" + string(dend_value) + raw";"
     DEND_string = "DEND\\$dend_count;"
     push!(HMDIF_out, DEND_string)
@@ -88,7 +107,7 @@ close(file)
 # Print the first line
 println(first_line)
 
-#Use the string after the word file as the survey name
+#Use the string after the word 'file' as the survey name
 
 survey_name = split(first_line, r"^.+file ")[:2]
 
@@ -101,11 +120,12 @@ survey_output_file = string(replace(survey_name, " " => "_") * ".HMD")
 print(survey_output_file)
 
 #now go and get the full file skipping the first 22 rows as they are the explanation of the codes.
+grid_file_name = "Test Grid 2.grd"
 
 grid_data = CSV.read(grid_file_name, DataFrame; header=22,
                                                 silencewarnings=true)
 
-grid_data
+#grid_data
 
 HMD_output = fn_build_hmdif(grid_data, survey_name) 
 
