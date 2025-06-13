306656a2b4305b1e6d2f4bd2440e05347ac1f0db
rj3838 <roy.jaques@live.co.uk>
2025-06-13 09:19:57 +0100

    splitting out function code to seperate files

---
+++ find_rows_with_value.jl
@@ -0,0 +1,16 @@
+function find_rows_with_value(section_df::DataFrame, cvi_code::String)
+    # remove the sectionID, Chainage and sectionNr columns as the string used for the cvi code can occur in 
+    # those columns.
+    #print("find_rows_with_value")
+     
+    rows_with_value = findall(row -> any(x -> x == cvi_code, row), eachrow(select(section_df, Not([:SectionID, :Chainage]))))
+
+    #p#rintln(typeof((section_df[rows_with_value, :]))) #0
+    #println("typeof ",typeof(rows_with_value))
+    row_number = size(section_df[rows_with_value, :],1)
+    #println(row_number)
+    #filter(row -> any(x -> x == value, row), eachrow(df))
+    #@where(df, findall(x -> x == value))
+    #print("exit find_rows_with_value")
+    return row_number
+end
